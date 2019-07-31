745b2f7a6f185d85657c4140001a977a6af59c3c
Chillhumanoid <jonathanthorne13@gmail.com>
2019-07-31 09:13:25 -0400

    Moved over search from localHelp to own script

--- search.py
+++ search.py
@@ -1,5 +1,35 @@
 import os
 import sys
 
+base = "C:/Users/jonat/OneDrive/Documents/JETS/"
+
 def article(): #for searching articles
-    
+    if len(sys.argv) < 4:
+        print()
+        print("Please enter a search term.")
+    else:
+        print()
+        x = 0
+        term = sys.argv[3]
+        if len(sys.argv) > 4:
+            for x in range(3,len(sys.argv)):
+                term = term + " " + sys.argv[x]
+        for folder in os.listdir(base):
+            if "Vol " in folder:
+                path = base + folder + "/"
+                for issue in os.listdir(path):
+                    nPath = path + issue + "/"
+                    for file in os.listdir(nPath):
+                        term = term.lower()
+                        f = file.lower()
+                        if term in f:
+                            aNum = file.split(")")[0]
+                            title = file.split(") - ")[1]
+                            vNum = folder.split(" ")[1]
+                            iNum = issue.split(".")[1]
+                            x = x + 1
+                            print(vNum + "." + iNum + "." + aNum + " - " + title)
+        if x == 0:
+            print()
+            print("No Articles Found")
+                        
