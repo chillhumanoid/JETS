6ef0f3dd65fbb01445b607e6e716b3ab491d0e29
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-03 15:56:41 -0400

    Added help doc for fix author

--- helpcmd.py
+++ helpcmd.py
@@ -8,6 +8,8 @@ def help():
         print("      -l                Prints all articles in all volumes")
         print("      -l (1-62)         Prints all articles in given volume")
         print("      -l (1-62).(1-4)   Prints all articles in given volume and issue")
+        print("      -f (1-64)         Fixes the author info on all articles in given volume")
+        print("      -f (1-64).(1-4)   Fixes the author info on all articles in given issue")
         print()
     elif len(sys.argv) == 3:
         arg = sys.argv[2]
@@ -21,8 +23,15 @@ def help():
             #search
             x = 1
         elif arg == "-f":
-            #fix author command
-            x = 1
+            print()
+            print("Fix Author Command")
+            print()
+            print("Description: Goes through a set volume or issue and fixes the author information")
+            print("             on all the articles. May not be necessary anymore")
+            print()
+            print("Usage:")
+            print("      -f (1-64)         Fixes the author info on all articles in given volume")
+            print("      -f (1-64).(1-4)   Fixes the author info on all articles in given issue")
         elif arg == "-fr":
             #force rename
             x = 1 
