ddb93e7c789b86f56d77c53117e0524b90b3ed15
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-10 17:57:31 -0400

    Search works with new cli, also better at searching

--- search.py
+++ search.py
@@ -5,7 +5,8 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 
 #global variables
 
-base = "C:/Users/jonat/OneDrive/Documents/JETS/"
+rPath = os.path.realpath(__file__)
+rPath = rPath.replace("search.py","")
 
 #functions
 def main():
@@ -16,69 +17,62 @@ def main():
         if arg == "-a":
             searchAuthors()
 
-def searchAuthors():
+def searchAuthors(author):
+    name = author.split(" ")
+    x = 0
+    listoNames = []
     articles = []
-    if len(sys.argv) < 4:
-        print()
-        print("Please enter a name")
-    else:
-        x = 0
-        listoNames = []
-        name = [sys.argv[3]]
-        if len(sys.argv) > 4:
-            for x in range(4, len(sys.argv)):
-                name.append(sys.argv[x])
-        for x in name:
-            for author in os.listdir(base + "Authors/"):
-                names = author.lower().split(" ")
-                if x.lower() in names:
-                    if not author in listoNames:
-                        listoNames.append(author)
+    for x in name:
+        for author in os.listdir(rPath + "Articles/Authors/"):
+            names = author.lower().split(" ")
+            if x.lower() in names:
+                if not author in listoNames:
+                    listoNames.append(author)
                 else:
                     if author in listoNames:
                         listoNames.remove(author)
-        for author in os.listdir(base + "Authors/"):
+        for author in os.listdir(rPath + "Articles/Authors/"):
             if author in listoNames:
-                for article in os.listdir(base + "Authors/" + author):
+                for article in os.listdir(rPath + "Articles/Authors/" + author):
                     articles.append(article)
         printFiles(articles)
 
-def searchArticles():    #for searching articles
-    path = base + "All/"
-    if len(sys.argv) < 4:    #If under 4, no search term given
-        print()   #for formatting
-        print("Please enter a search term.")
-
-    else:
-        found = []
-        print() #for formatting
-        x = 0   #used in loop iterations
-        term = ' '.join(sys.argv[3:])#search term starts at point 3
-        term = term.lower()
-        if len(sys.argv) == 4:  #if the length is 4 exactly
-            for article in os.listdir(path):  #goes through all folders in base
-                f = article.lower()
-                if term in f:
-                    found.append(article)
-        elif len(sys.argv) > 4:
+def searchArticles(term):    #for searching articles
+    path = rPath + "Articles/All/"
+    found = []
+    click.echo() #for formatting
+    x = 0   #used in loop iterations
+    term = term.lower()
+    if len(term) == 1:  #if the length is 4 exactly
+        for article in os.listdir(path):  #goes through all folders in base
+            f = article.lower()
+            if term in f:
+                found.append(article)
+    elif len(term) > 1:
+        for article in os.listdir(path):
+            f = article.lower()
+            if term in f:    #if term is found
+                found.append(article)
+        if len(found) == 0:
             for article in os.listdir(path):
+                terms = term.split(" ")
                 f = article.lower()
-                if term in f:    #if term is found
-                    found.append(article)
-            if len(found) == 0:
-                for article in os.listdir(path):
-                    terms = term.split(" ")
-                    f = article.lower()
-                    for y in terms:
-                        y = y.lower()
-                        if y == "the" or y == "of" or y == "a" or y == "and" or y == "or" or y == "if" or y == "&" or y == "is" or y == "on": #ignores major key words, otherwise everything would be displayed
-                            temporary = 0
-                        elif y in f:
-                            found.append(article)
-        printFiles(found)
+                run = 1
+                for y in terms:
+                    y = y.lower()
+                    if y == "the" or y == "of" or y == "a" or y == "and" or y == "or" or y == "if" or y == "&" or y == "is" or y == "on": #ignores major key words, otherwise everything would be displayed
+                        temporary = 0
+                    elif y in f and run == 1:
+                        found.append(article)
+                        run = run + 1
+                    if y not in f:
+                        run = run + 1
+                        if article in found:
+                            found.remove(article)
+    printFiles(found)
 
 def printFiles(articles):
-    path = base + "All/"
+    path = rPath + "Articles/All/"
     if not len(articles) == 0:
         u = 0
         for x in articles:
