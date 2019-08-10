745d0962b53750b3dedea5fecd46eca42a2114a5
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-10 19:47:01 -0400

    better number checker for ..well..numbers

--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-import os, click
+import os, click, sys
 
 
 path = os.path.realpath(__file__)
@@ -18,6 +18,16 @@ def p(msg):
     click.echo()
     click.echo(msg)
 
+def check_vol(vol):
+    if not vol >= 0 and vol <= 62:
+        p("Please enter 1-62 for Volume")
+        sys.exit()
+
+def check_issue(issue):
+    if not issue >= 0 and vol <= 4:
+        p("Please enter 1-4 for Issue")
+        sys.exit()
+
 def getNumbers(term):
     cDot = term.count(".")
     vNum = "0"
@@ -25,8 +35,21 @@ def getNumbers(term):
     aNum = "0"
     if cDot >= 0:
         vNum = term.split(".")[0]
+        if vNum.isdigit():
+            check_vol(int(vNum))
+        else:
+            p("Please enter a number for volume")
+            sys.exit()
     if cDot >= 1:
         iNum = term.split(".")[1]
+        if iNum.isdigit():
+            check_issue(int(iNum))
+        else:
+            p("Please enter a number for issue")
+            sys.exit()
     if cDot == 2:
         aNum = term.split(".")[2]
+        if not aNum.isdigit():
+            p("please enter a number for article")
+            sys.exit()
     return (vNum, iNum, aNum)
