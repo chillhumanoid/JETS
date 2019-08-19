03c071a3a19639aacc3cfbe3d68eedb6b606fafb
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-19 10:52:57 -0400

    cleaned up text, added a quotifier

--- jets_cli/database.py
+++ jets_cli/database.py
@@ -3,38 +3,36 @@ path = os.path.realpath(__file__)
 path = path.replace("database.py","")
 path = path + "Articles/"
 
-def create_database():
-    print(path)
-    conn = sqlite3.connect(path + 'author.db')
+def sql_executor(sql):
+    conn = sqlite3.connect(path + "author.db")
     c = conn.cursor()
+    c.execute(sql)
+    conn.commit()
+    return c
+def create_database():
     sql = """CREATE TABLE IF NOT EXISTS authors (
         id integer PRIMARY KEY,
         name text NOT NULL,
         articlenums text
     );"""
-    c.execute(sql)
-    conn.close()
+    sql_executor(sql)
     
 def get_names():
     names = []
     sql = "SELECT name FROM authors"
-    conn = sqlite3.connect(path + "author.db")
-    c = conn.cursor()
-    c.execute(sql)
+    c = sql_executor(sql)
     for x in c:
         names.append(x[0])
     return names
 
 def search_table(author_name):
-    sql = "SELECT * FROM authors WHERE name = %s" % author_name
-    conn = sqlite3.connect(path + "author.db")
-    c = conn.cursor()
-    data = c.execute(sql).fetchall()
+    sql = "SELECT * FROM authors WHERE name = %s" % quotify(author_name)
+    c = sql_executor(sql)
+    data = c.fetchall()
     if len(data) == 0:
         value = False
     else:
         value = True
-    conn.close()
     return value
 
 def sort_articles(new_number, existing_numbers):
@@ -48,7 +46,6 @@ def sort_articles(new_number, existing_numbers):
         existing_vol = number.split(".")[0]
         existing_index = number.split(".")[1]
         existing_article = number.split(".")[2]
-        print(existing_vol)
         if new_vol == existing_vol:
             if new_index == existing_index:
                 if new_article == existing_article:
@@ -73,16 +70,24 @@ def sort_articles(new_number, existing_numbers):
             numbers.insert(replace_position, new_number)
     return numbers
 
-def add_to_table(author_name, article_nums):
-    author_name = "'" + author_name + "'"
+def rename_author(id, new_author_name):
+    print(new_author_name)
+    print(id)
+    sql = "UPDATE authors SET name = %s WHERE id = %s" %(quotify(new_author_name), id)
+    sql_executor(sql)
+
+def get_id(author_name):
+    sql = "SELECT id FROM authors WHERE name = %s" % quotify(author_name)
+    c = sql_executor(sql).fetchall()
+    return c[0][0]
+
+def add_to_table(author_name, article_numbers):
     if search_table(author_name):
-        full_num = sort_articles(article_nums, get_numbers(author_name))
-        full_num = ";".join(full_num)
-        full_num = "'" + full_num + "'"
-        sql = "UPDATE authors SET articlenums = %s WHERE name = %s" % (full_num, author_name)
+        full_number = sort_articles(article_numbers, get_numbers(author_name))
+        full_number = ";".join(full_number)
+        sql = "UPDATE authors SET articlenums = %s WHERE name = %s" % (quotify(full_number), quotify(author_name))
     else:
-        article_nums = "'" + article_nums + "'"
-        sql = "INSERT INTO authors(name, articlenums) VALUES (%s, %s)" % (author_name, article_nums)
+        sql = "INSERT INTO authors(name, articlenums) VALUES (%s, %s)" % (quotify(author_name), quotify(article_numbers))
     conn = sqlite3.connect(path + "author.db")
     c = conn.cursor()
     c.execute(sql)
@@ -93,37 +98,29 @@ def print_table():
     conn = sqlite3.connect(path + "author.db")
     c = conn.cursor()
     for row in c.execute('SELECT * FROM authors ORDER BY name ASC'):
-        print(row)
+        click.echo(row)
     conn.close()
 
 def remove_author(author_name):
-    sql = "DELETE FROM authors WHERE name = %s" % "'" + author_name + "'"
-    conn = sqlite3.connect(path + "author.db")
-    c = conn.cursor()
-    c.execute(sql)
-    conn.commit()
-    conn.close()
+    sql = "DELETE FROM authors WHERE name = %s" % quotify(author_name)
+    sql_executor(sql)
 
 def get_numbers(author_name):
-    sql = "SELECT articlenums FROM authors WHERE name = %s" % author_name
-    conn = sqlite3.connect(path + "author.db")
-    c = conn.cursor()
-    data = c.execute(sql).fetchall()
-    data = data[0]
-    full_num = data[0]
-    full_num = full_num.split("'")[0]
-    conn.close()
-    return full_num
+    sql = "SELECT articlenums FROM authors WHERE name = %s" % quotify(author_name)
+    c = sql_executor(sql)
+    data = c.fetchall()
+    full_number = data[0][0]
+    full_number = full_number.split("'")[0]
+    return full_number
 
 def get_full_numbers(author_name):
-    sql = "SELECT articlenums FROM authors WHERE name = %s" % author_name
-    conn = sqlite3.connect(path + "author.db")
-    c = conn.cursor()
-    data = c.execute(sql)
-    data = data.fetchall()
-    data = data[0]
-    full_num = data[0]
-    full_nums = full_num.split(";")
+    sql = "SELECT articlenums FROM authors WHERE name = %s" % quotify(author_name)
+    c = sql_executor(sql)
+    data = c.fetchall()
+    full_number = data[0][0]
+    full_numbers = full_number.split(";")
+    return full_numbers
 
-    conn.close()
-    return full_nums
+def quotify(string):
+    string = "'" + string + "'"
+    return string
