619e835a88664d850b2ed224bcfaa017373da632
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-18 21:02:40 -0400

    open now uses database

--- jets_cli/open.py
+++ jets_cli/open.py
@@ -1,33 +1,30 @@
-import os, sys, subprocess, click
+import os, sys, subprocess, click, database
 from display import display
 
 path = os.path.realpath(__file__)
 path = path.replace("open.py","")
-fPath = path + "Articles/All/"
-aPath = path + "Articles/Authors/"
+all_path = path + "Articles/All/"
 
-def open_author(auth):
+
+def open_author(author_name):
     articles = []
-    aPaths = []
-    x = 0
-    for author in os.listdir(aPath):
-        if auth.lower() in author.lower():
-            x = x + 1
-            for article in os.listdir(aPath + author):
-                articles.append(article)
-                aPaths.append(aPath + author + "/" + article)
-    d(articles)
-    if click.confirm("Open All?"):
-        for pth in aPaths:
-            os.startfile(pth)
-    if x==0:
+    if database.search_table(author_name):
+        for number in database.get_full_numbers(author_name):
+            for article in os.listdir(all_path):
+                if article.startswith(number):
+                    articles.append(article)
+        display(articles)
+        if click.confirm("Open All?"):
+            for article in articles:
+                os.startfile(all_path + article)
+    else:
         print("Author Not Found")
 
 def open_file(num):
     x = 0
-    for article in os.listdir(fPath):
+    for article in os.listdir(all_path):
         if num in article:
-            os.startfile(fPath + article)
+            os.startfile(all_path + article)
             x = x + 1
     if x == 0:
         print("Article Not Found")
