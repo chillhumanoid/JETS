d108e49ba080a0fa2ca207fa14e4ab375935db0e
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-22 12:26:16 -0400

    download now uses login info for locked archives

--- jets_cli/downloader.py
+++ jets_cli/downloader.py
@@ -1,5 +1,6 @@
-import requests, urllib.request, sqlite3, known_authors, time, os, sys, click, util, rename as ren, fileparser as parsr, database
+import requests, urllib.request, sqlite3, known_authors, time, os, sys, click, util, rename as ren, fileparser as parsr, database, login as log
 from bs4 import BeautifulSoup
+from requests import Session
 from pathvalidate import ValidationError, validate_filename
 from PyPDF2 import PdfFileReader, PdfFileWriter; from shutil import copyfile, move
 
@@ -15,84 +16,90 @@ def start(data):
     get_volume_url(data)
 
 def get_volume_url(data):
-    volume_number = data[0]
-    if volume_number == '0':
-        file_start = "Vol "
-    else:
-        file_start = " " + volume_number + " "
-
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text,'html.parser')
-
-    link_list = soup.findAll('a') #finds the "a" tag on url
-    for link in link_list:
-        link = str(link)
-        if file_start in link: 
-            link_start = link.find('"') + 1
-            link_end = link.find('"', link_start)
-            urlAppend = link[link_start:link_end]
-            volume_url = base_url + urlAppend
-            get_issue_url(data, volume_url)
-
-def get_issue_url(data, volume_url):
-    issue_number_original = data[1]
-    issue_number = data[1]
-    volume_number = data[0]
-
-    response = requests.get(volume_url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    link_list = soup.findAll('a')
-
-    i = 0
-    for link in link_list:
-        link = str(link)
-        if volume_number + "_" in link and "Go to" not in link:
-            link_start = link.find('"') + 1
-            link_end = link.find('"', link_start)
-            url_append = link[link_start:link_end]
-            i_position = url_append.find("_") + 1
-            i = url_append[i_position:i_position+1]
-            issue_url = base_url + url_append
-            if issue_number == '0' or "." + issue_number in link:
-                data[1] = str(i)
-                get_article_url(data, issue_url)
-                data[1] = issue_number_original
-
-def get_article_url(data, issue_url):
-    article_number_original = data[2]
-    article_number = data[2]
-
-    response = requests.get(issue_url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    link_list = soup.findAll('a')
-    
-    z = 0
-    for link in link_list:
-        link = str(link)
-        if ".pdf" in link and "Purchase Articles" not in link and "Purchase Back Issue(s)" not in link:
-            z = z + 1
-            if str(z) == article_number or article_number == '0':
-                if "<em>" in link:
-                    link = link.replace("<em>", "")
-                    link = link.replace("</em>", "")
-                if "<br/>" in link:
-                    link = link.replace("<br/>", "")
-                    link = link.replace("\n", '')
-                title_start = link.find('>') + 1
-                title_end = link.find('<', title_start)
+    with Session() as s:
+        login_data = {"name":log.get_username(), "pass":log.get_password(), "op":"Log in", "form_build_id":"form-a0ed7b5c7437ac9afeb21b126e24633b", "form_id":"user_login_block"}
+        s.post("https://www.etsjets.org/new_welcome?destination=node%2F1120", login_data)
+        volume_number = data[0]
+        if volume_number == '0':
+            file_start = "Vol "
+        else:
+            file_start = " " + volume_number + " "
+ 
+        response = s.get(url)
+        soup = BeautifulSoup(response.content,'html.parser')
+ 
+        link_list = soup.findAll('a') #finds the "a" tag on url
+        for link in link_list:
+            link = str(link)
+            if file_start in link:
                 link_start = link.find('"') + 1
                 link_end = link.find('"', link_start)
-                article_url = link[link_start:link_end]
-                article_url = base_url + article_url
-                title = link[title_start:title_end]
-                if "http://www.etsjets.org/" in article_url:
-                    article_url = article_url.replace("http://www.etsjets.org", "")
-                if "https://www.etsjets.org/" in article_url:
-                    article_url = article_url.replace("https://www.etsjets.org", "")
-                article_url = base_url + article_url
-                data[2] = str(z)
-                get_title_and_author(data, title, article_url)
-                data[2] = article_number_original
+                urlAppend = link[link_start:link_end]
+                volume_url = base_url + urlAppend
+                get_issue_url(data, volume_url)
+ 
+def get_issue_url(data, volume_url):
+    with Session() as s:
+        login_data = {"name":log.get_username(), "pass":log.get_password(), "op":"Log in", "form_build_id":"form-a0ed7b5c7437ac9afeb21b126e24633b", "form_id":"user_login_block"}
+        s.post("https://www.etsjets.org/new_welcome?destination=node%2F1120", login_data)
+        issue_number_original = data[1]
+        issue_number = data[1]
+        volume_number = data[0]
+ 
+        response = s.get(volume_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        link_list = soup.findAll('a')
+ 
+        i = 0
+   
+        for link in link_list:
+            link = str(link)
+            if (" " + volume_number + "." in link or volume_number + "." in link) and not "Go to " in link:
+                i = i + 1
+                link_start = link.find('"') + 1
+                link_end = link.find('"', link_start)
+                url_append = link[link_start:link_end]
+                issue_url = base_url + url_append
+                if issue_number == '0' or "." + issue_number in link or "_" + issue_number in link:
+                    data[1] = str(i)
+                    get_article_url(data, issue_url)
+                    data[1] = issue_number_original
+ 
+def get_article_url(data, issue_url):
+    with Session() as s:
+        login_data = {"name":log.get_username(), "pass":log.get_password(), "op":"Log in", "form_build_id":"form-a0ed7b5c7437ac9afeb21b126e24633b", "form_id":"user_login_block"}
+        s.post("https://www.etsjets.org/new_welcome?destination=node%2F1120", login_data)
+        article_number_original = data[2]
+        article_number = data[2]
+ 
+        response = s.get(issue_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        link_list = soup.findAll('a')
+   
+        z = 0
+        for link in link_list:
+            link = str(link)
+            if ".pdf" in link and "Purchase Articles" not in link and "Purchase Back Issue(s)" not in link:
+                z = z + 1
+                if str(z) == article_number or article_number == '0':
+                    if "<em>" in link:
+                        link = link.replace("<em>", "")
+                        link = link.replace("</em>", "")
+                    title_start = link.find('>') + 1
+                    title_end = link.find('<', title_start)
+                    link_start = link.find('"') + 1
+                    link_end = link.find('"', link_start)
+                    article_url = link[link_start:link_end]
+                    article_url = base_url + article_url
+                    title = link[title_start:title_end]
+                    if "http://www.etsjets.org/" in article_url:
+                        article_url = article_url.replace("http://www.etsjets.org", "")
+                    if "https://www.etsjets.org/" in article_url:
+                        article_url = article_url.replace("https://www.etsjets.org", "")
+                    article_url = base_url + article_url
+                    data[2] = str(z)
+                    get_title_and_author(data, title, article_url)
+                    data[2] = article_number_original
 
 def get_title_and_author(data, title, article_url):
     original_file_name = title
