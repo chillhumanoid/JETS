515437c1b098a3be83597614ec7bec61500a5200
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-22 12:28:39 -0400

    changed for new code since last touched

--- jets_cli/merge.py
+++ jets_cli/merge.py
@@ -1,10 +1,10 @@
-import os, sys
+import os, sys, util
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 path = os.path.realpath(__file__)
 path = path.replace("merge.py","")
 merge_path = path + "Articles/Merged/"
-path = path + "Articles/All/"
+all_path = path + "Articles/All/"
 
 
 def merge(vol_num, issue_num):
@@ -13,25 +13,24 @@ def merge(vol_num, issue_num):
         issues = []
         for file in os.listdir(path):
             if file.startswith(vol_num + "."):
-                title = util.getInfo(path + file)[0]
                 num = util.get_nums(file)[0]
                 issues.append(num)
         for issue_num in issues:
-            merge_issues(vol_num, issue_num)
+            merge_issues(folder, vol_num, issue_num)
         merge_vol(vol_num)
     else:
-        merge_issues(vol_num, issue_num)
+        merge_issues(folder, vol_num, issue_num)
 
-def merge_issues(vol_num, issue_num):
-    merge_path = merge_path + "Vol " + vol_num + "/"
-    if not os.path.exists(merge_path):
-        os.mkdir(merge_path)
-    output = merge_path + "Issue " + str(issue_num) + ".pdf"
+def merge_issues(folder, vol_num, issue_num):
+    merged_path = merge_path + folder
+    if not os.path.exists(merged_path):
+        os.mkdir(merged_path)
+    output = merged_path + "Issue " + str(issue_num) + ".pdf"
     file_out = open(output, 'ab')
     writer = PdfFileWriter()
     for article in os.listdir(path):
         if article.startswith(vol_num + "." + issue_num):
-            file = open(fPath + article, 'rb')
+            file = open(all_path + article, 'rb')
             pdf = PdfFileReader(file)
             for page in range(pdf.getNumPages()):
                 writer.addPage(pdf.getPage(page))
@@ -46,7 +45,7 @@ def merge_vol(vol_num):
             file_out = open(output, 'ab')
             writer = PdfFileWriter()
             for file in os.listdir(merge_path + folder):
-                pdf_file = open(merge_path + folders + "/" + file, 'rb')
+                pdf_file = open(merge_path + folder + "/" + file, 'rb')
                 pdf = PdfFileReader(pdf_file)
                 for page in range(pdf.getNumPages()):
                     writer.addPage(pdf.getPage(page))
