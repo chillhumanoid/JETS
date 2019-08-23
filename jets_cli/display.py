d70f99f519c5f8360e65bfb7a60fc54191a9e661
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-22 22:38:09 -0400

    fixed display

--- jets_cli/display.py
+++ jets_cli/display.py
@@ -1,8 +1,10 @@
-import util, click, os
+import util, click, os, shutil, database as db
 
 path = os.path.realpath(__file__)
 path = path.replace("display.py","")
 path = path + "Articles/All/"
+
+columns, rows = shutil.get_terminal_size(fallback=(80, 24))
 
 def display(articles):
     title_display_len = get_longest(articles) + 2
@@ -17,10 +19,13 @@ def get_longest(articles):
         z = len(title)
         if z > u:
             u = z
+    col = columns - 43
+    if u > col:
+        u = col
     return u
 
 def display_header(title_len):
-    header = "{0:^11}|  {1:^{3}}|{2:^16}".format("ARTICLE", " TITLE", "AUTHOR", title_len)
+    header = "{0:^11}|  {1:^{3}}|{2:^30}".format("ARTICLE", " TITLE", "AUTHOR", title_len)
     lines = get_lines(header, title_len)
     line, line2 = lines[0], lines[1]
     util.p(line)
@@ -31,7 +36,7 @@ def get_lines(header, title_len):
     lChar = u"\u2015"
     line = ""
     line2 = ""
-    for x in range(len(header) + 8): #seems self explanatory
+    for x in range(len(header)): #seems self explanatory (update: no it doesnt)
         if x == 11 or x == 13 + title_len:
             line2 = line2 + "|"
             line = line + lChar
@@ -41,43 +46,39 @@ def get_lines(header, title_len):
 
 def display_articles(articles, title_len):
     title_len = int(title_len)
+    max_length = columns - 43
     for article in articles:
             num = util.get_nums(article)[0]
             title = util.get_info(path + article)[0]
-            author = util.get_info(path + article)[1]
-            display = "{0:^11}|  {1:<{3}}|  {2}".format(num, title, author, title_len)
+            author_list = db.get_author(num)
+            if len(author_list) > 1:
+                author = get_authors(author_list)
+            else:
+                author = author_list[0]
+            if len(author) > 30:
+                author = author[:27] + "..."
+            if len(title) > max_length:
+                title = title[:max_length] 
+            display = "{0:^11}|  {1:<{3}}| {2:<30}".format(num, title, author, title_len)
             click.echo(display)
 
-def get_authors(author):
-    a = ''
-    if " And " in author:
-        authors = []
-        auth_split = author.split(" And ")
-        for name in auth_split:
-            if "," in name:
-                auth_split_again = a.split(",")
-                for auth in auth_split_again:
-                    auth = util.string_strip()
-                    if not auth == "":
-                        authors.append(auth)
-                    else:
-                        authors.append(a)
-            fullName = []
-            for name in authors:
-                first = name[0:1] + "."
-                full_name = name.split(" ")
-                full_name[0] = first
-                if "Jr" in full_name or "III" in full_name:
-                    last_location = len(full_name) - 2
-                else:
-                    last_location = len(full_name) - 1
-                    last = full_name[last_location]
-                    if not last_location == 1:
-                        full_name = get_middle(last_location, full_name)
-                    a = ' '.join(full_name)
-                    fullName.append(a)
-                author = ', '.join(fullName)
-            return author
+def get_authors(author_list):
+    fullName = []
+    for name in author_list:
+        first_name = name[0:1] + "."
+        full_name = name.split(" ")
+        full_name[0] = first_name
+        if "Jr" in full_name or "III" in full_name:
+            last_location = len(full_name) - 2
+        else:
+            last_location = len(full_name) - 1
+            last_name = full_name[last_location]
+            if not last_location == 1:
+                full_name = get_middle(last_location, full_name)
+            name = ' '.join(full_name)
+            fullName.append(name)
+    author = ', '.join(fullName)
+    return author
 
 def get_middle(location, full_name):
     middle_location = location - 1
