761e78731f06f76bbea9b6f215044c5e29d4ebae
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-23 16:34:38 -0400

    opens based on database info now

--- jets_cli/open.py
+++ jets_cli/open.py
@@ -7,23 +7,33 @@ all_path = path + "Articles/All/"
 
 
 def open_author(author_name):
-    articles = []
-    if database.search_table(author_name):
-        for number in database.get_full_numbers(author_name):
-            for article in os.listdir(all_path):
-                if article.startswith(number):
-                    articles.append(article)
-        display(articles)
-        if click.confirm("Open All?"):
-            for article in articles:
-                os.startfile(all_path + article)
-    else:
-        print("Author Not Found")
+    """
+    Open all articles by a specific author
 
-def open_file(num):
+    Parameters:
+    author_name (string)
+    """
+    author_exists = db.search_author_table(author_name)
+    if author_exists:
+        article_id_list = db.get_article_ids(author_name)
+        number_of_articles = str(len(article_id_list))
+        if click.confirm("Are you sure you want to open all " + number_of_articles + " articles by " + author_name + "?"):
+            for article_id in article_id_list:
+                os.startfile(all_path + str(article_id) + ".pdf")
+    else:
+        util.p("Author not found")
+
+def open_file(full_number):
+    """
+    Open a specific file based on the full number(volume.issue.article ##.##.##)
+
+    Parameters:
+    full_number (string)
+    """
     x = 0
+    article_id = db.get_article_id(full_number)
     for article in os.listdir(all_path):
-        if num in article:
+        if article_id + ".pdf" == article:
             os.startfile(all_path + article)
             x = x + 1
     if x == 0:
