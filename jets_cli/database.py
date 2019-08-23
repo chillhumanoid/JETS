d27d30abd489108c8d96598fd2d48a737003d2ec
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-23 16:29:54 -0400

    Getters of articles based on volume and issue

--- jets_cli/database.py
+++ jets_cli/database.py
@@ -305,6 +305,26 @@ def get_full_number(article_id):
 
 
 
+def get_article_by_volume_number(volume_number):
+    sql = "SELECT article_id FROM titles WHERE volume_number = %s ORDER BY full_number ASC" % volume_number
+    c = sql_executor(sql).fetchall()
+    article_list = []
+    for item in c:
+        article_list.append(item[0])
+    return article_list
+
+
+
+def get_article_by_issue_number(volume_number, issue_number):
+    sql = "SELECT article_id FROM titles WHERE volume_number = %s and issue_number = %s ORDER BY full_number ASC" % (volume_number, issue_number)
+    c = sql_executor(sql).fetchall()
+    article_list = []
+    for item in c:
+        article_list.append(item[0])
+    return article_list
+
+
+
     """
     gets article_ids based on author name
 
