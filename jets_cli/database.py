753b7deb2fa0bd63819c9c9d6ddf598e85e1853c
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-23 16:29:04 -0400

    dev based functions, only directly accessible

--- jets_cli/database.py
+++ jets_cli/database.py
@@ -267,12 +267,24 @@ def get_author(full_number):
         author_list.append(name)
     return author_list
 
-def print_table():
+
+
+def print_author_table(): #DEV USE
     conn = sqlite3.connect(path + "author.db")
     c = conn.cursor()
     for row in c.execute('SELECT * FROM authors ORDER BY name ASC'):
         click.echo(row)
     conn.close()
+
+
+
+def print_article_table(): #DEV USE
+    conn = sqlite3.connect(path + "author.db")
+    c = conn.cursor()
+    for row in c.execute("SELECT * FROM titles ORDER BY full_number ASC"):
+        click.echo(row)
+    conn.close()
+
 
     """
     Gets the full number based on article id
