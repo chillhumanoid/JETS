70ad63a62f4a872d3c7ab86b070947227b3ca01b
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-23 16:34:02 -0400

    uses database now

--- jets_cli/lister.py
+++ jets_cli/lister.py
@@ -17,8 +17,10 @@ def start(volume_number, issue_number):
         display(article_ids)
     if not volume_number == "0" and issue_number == "0":
         volume_number = util.check_digit(volume_number)
-        display([article for article in os.listdir(path) if article.startswith(volume_number + ".")])
+        article_ids = db.get_article_by_volume_number(int(volume_number))
+        display(article_ids)
     if not volume_number == "0" and not issue_number == "0":
         volume_number = util.check_digit(volume_number)
         issue_number = util.check_digit(issue_number)
-        display([article for article in os.listdir(path) if article.startswith(volume_number + "." + issue_number + ".")])
+        article_ids = db.get_article_by_issue_number(int(volume_number), int(issue_number))
+        display(article_ids)
