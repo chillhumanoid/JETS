41a9833895e0aac7a3f1d03af46b98d6d0e56536
Chillhumanoid <jonathanthorne13@gmail.com>
2019-08-25 01:57:59 -0400

    fixed all_path

--- jets_cli/database.py
+++ jets_cli/database.py
@@ -2,7 +2,7 @@ import sqlite3, os, click, util, sys, time
 path = os.path.realpath(__file__)
 path = path.replace("database.py","")
 path = path + "Articles/"
-all_path = path + "/All"
+all_path = path + "All/"
 
 def sql_executor(sql):
     """
