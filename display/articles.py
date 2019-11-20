6e77550e468a21628e03a7d759b4a3278e42cbaa
Chillhumanoid <jonathanthorne13@gmail.com>
2019-11-19 22:36:30 -0500

    Updated some of the logic on article viewing

--- display/articles.py
+++ display/articles.py
@@ -1,9 +1,9 @@
 from menus import by_issue
 import sys, curses, os
-from utilities import get_year as get, names
+from utilities import get_year as get, names, open
 from database import get_numbers, get_article, get_title, get_author
 from math import *
-
+from display import article
 
 def menu(stdscr, volume_number, year, issue_number):
     x_start_pos = 2
@@ -53,24 +53,31 @@ def menu(stdscr, volume_number, year, issue_number):
 
             rows = len(article_ids)
             max_rows = height - 4
-            last_row = rows - (max_rows * (max_pages - 1)) + 2
+            if rows > max_rows:
+                num_pages = ceil(rows/max_rows)
+                last_row = rows - (max_rows * (num_pages - 1)) + 2
+            else:
+                num_pages = 1
+                last_row = rows - 1
+            
 
 
             if k == curses.KEY_UP:
                 cursor_y -= 1
                 if cursor_y == 0:
-                    if current_page == max_pages:
+                    if current_page == num_pages:
                         cursor_y = last_row
                     else:
                         cursor_y = max_rows - 1
             elif k == curses.KEY_DOWN:
                 cursor_y += 1
-                if current_page == max_pages:
+                if current_page == num_pages:
                     if cursor_y == last_row + 1:
                         cursor_y = 1
                 else:
                     if cursor_y == max_rows:
                         cursor_y = 1
+
             articles = []
 
 
@@ -105,6 +112,8 @@ def menu(stdscr, volume_number, year, issue_number):
                             magic_number = 78 - len(title)
                             magic_string = " " * magic_number
                             title = title + magic_string
+                        if len(display_author) > 26:
+                            display_author = display_author[:23] + " . . ."
                         display_string = "{}| {} | {}".format(number, title, display_author)
                         if cursor_y == y_position:
                             stdscr.attron(curses.color_pair(3))
@@ -129,6 +138,8 @@ def menu(stdscr, volume_number, year, issue_number):
                                 magic_number = 78 - len(title)
                                 magic_string = " " * magic_number
                                 title = title + magic_string
+                            if len(author) > 25:
+                                author = author[:22] + " . . ."
                             display_string = "{}| {} | {}".format(number, title, display_author)
                             if cursor_y == y_position:
                                 stdscr.attron(curses.color_pair(3))
@@ -139,18 +150,42 @@ def menu(stdscr, volume_number, year, issue_number):
                     status_bar = " Press 'n' to go to the next page | Press 'p' to go to the previous page | Press 'esc' to go to issue_selection"
                     for i in range(0, max_rows -1):
                         y_position = i + 1
-                        article = articles[i + (max_rows * (current_page - 1)) - 1]
-                        article = "{}) {}".format(y_position, article)
-                        if len(article) > 40:
-                            article = article[:40]
-                        display_string = "{} | "
-                        if cursor_y == y_position:
-                            stdscr.attron(curses.color_pair(3))
-                        stdscr.addstr(y_position, x_start_pos, display_string)
-                        if cursor_y == y_position:
-                            stdscr.attroff(curses.color_pair(3))
+                        check = i + (max_rows * (current_page-1)) - 1
+                        if not check > rows:
+                            article_id = article_ids[i + (max_rows * (current_page-1)) - 2]
+                            number = get_numbers.full(article_id)
+                            number = number.split(".")[1] + "." + number.split(".")[2]
+                            title = get_title.by_article_id(article_id)
+                            author = get_author.by_article_id(article_id)
+                            display_author = names.get_authors(author)
+                            if len(title) > 75:
+                                title = title[:72] + " . . ."
+                            else:
+                                magic_number = 78 - len(title)
+                                magic_string = " " * magic_number
+                                title = title + magic_string
+                            if len(author) > 25:
+                                author = author[:22] + " . . ."
+                            display_string = "{}| {} | {}".format(number, title, display_author)
+                            if cursor_y == y_position:
+                                stdscr.attron(curses.color_pair(3))
+                            stdscr.addstr(y_position, x_start_pos, display_string)
+                            if cursor_y == y_position:
+                                stdscr.attroff(curses.color_pair(3))
+                i = cursor_y - 1
+                if current_page == max_pages:
+                    article_id = article_ids[i + (max_rows * (current_page -1)) - 2]
+                elif(current_page == 1):
+                    article_id = article_ids[i]
+                else:
+                    article_id = article_ids[i + (max_rows * (current_page -1)) - 1]
+                if k == ord('o') or k == 10:
+                    full_number = get_numbers.full(article_id)
+                    open.open_file(full_number)
+                elif k == ord('i'):
+                    article.start(article_id, volume_number, year, issue_number)
             else:
-                l_row = "Page 1 of 1"
+                l_row = "Page {} of {}".format(current_page, num_pages)
                 if k == curses.KEY_LEFT:
                     by_issue.start(volume_number, year)
                 for i in range(0, rows - 1):
@@ -173,6 +208,15 @@ def menu(stdscr, volume_number, year, issue_number):
                     stdscr.addstr(y_position, x_start_pos, display_string)
                     if cursor_y == y_position:
                         stdscr.attroff(curses.color_pair(3))
+                i = cursor_y - 1
+                print(cursor_y)
+                print(i)
+                article_id = article_ids[i]
+                if k == 10:
+                    article.start(article_id, volume_number, year, issue_number)
+                elif k == ord('o'):
+                    full_number = get_numbers.full(article_id)
+                    open.open_file(full_number)
 
         stdscr.attron(curses.color_pair(3))
         stdscr.addstr(height-1, 0, status_bar)
