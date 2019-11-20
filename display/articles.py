8c72ec3dad70c02d0c2abae81ead25baeb62e1c5
Chillhumanoid <jonathanthorne13@gmail.com>
2019-11-20 11:37:52 -0500

    cleaned up code for articles

--- display/articles.py
+++ display/articles.py
@@ -1,13 +1,13 @@
 from menus import by_issue, main, by_volume_year
 import sys, curses, os
-from utilities import get_year as get, names, open
+from utilities import get_year as get, names, open, arith, string_handler
 from database import get_numbers, get_article, get_title, get_author
 from math import *
 from display import article
 
 
 def menu(stdscr, volume_number, year, issue_number, volume_current_page):
-    x_start_pos = 2
+    x_start_pos = 1
     cursor_y = 1
     cursor_x = 2
     k = 0
@@ -28,6 +28,8 @@ def menu(stdscr, volume_number, year, issue_number, volume_current_page):
     while(True):
         if k == 27:
             break
+        elif k == ord('q'):
+            sys.exit()
         else:
             stdscr.clear()
 
@@ -40,7 +42,7 @@ def menu(stdscr, volume_number, year, issue_number, volume_current_page):
 
             height, width = stdscr.getmaxyx()
 
-            start_x_title = int((width // 2) - (len(title) //2) - len(title) % 2)
+            start_x_title = arith.title_start(title, width)
 
             status_bar = " 'o' : Open | 'i' : Info | esc : Issue Selection | 'v' : Volume Selection | 'm' : Main Menu "
 
@@ -51,15 +53,11 @@ def menu(stdscr, volume_number, year, issue_number, volume_current_page):
             stdscr.attroff(curses.color_pair(1))
 
             rows = len(article_ids)
+            print("ROWS {}".format(rows))
             max_rows = height - 4
-            if rows > max_rows:
-                num_pages = ceil(rows/max_rows)
-                last_row = rows - (max_rows * (num_pages - 1)) + 2
-            else:
-                num_pages = 1
-                last_row = rows - 1
-
-
+            num_pages = arith.get_page_num(rows, max_rows)
+            last_row = arith.get_last_row(rows, max_rows, num_pages)
+            print("LAST ROW {}".format(last_row))
 
             if k == curses.KEY_UP:
                 cursor_y -= 1
@@ -81,10 +79,8 @@ def menu(stdscr, volume_number, year, issue_number, volume_current_page):
 
 
             if rows > max_rows:
-                num_pages = ceil(rows/max_rows)
-                max_pages = num_pages
 
-                if (k == ord('n') or k == curses.KEY_RIGHT) and not current_page == max_pages:
+                if (k == ord('n') or k == curses.KEY_RIGHT) and not current_page == num_pages:
                     cursor_y = 1
                     current_page += 1
                 elif (k == ord('p') or k == curses.KEY_LEFT) and not current_page == 1:
@@ -100,46 +96,21 @@ def menu(stdscr, volume_number, year, issue_number, volume_current_page):
                     for i in range(0, max_rows - 1):
                         y_position = i + 1
                         article_id = article_ids[i]
-                        number = get_numbers.full(article_id)
-                        number = number.split(".")[1] + "." + number.split(".")[2]
-                        title = get_title.by_article_id(article_id)
-                        author = get_author.by_article_id(article_id)
-                        display_author = names.get_authors(author)
-                        if len(title) > 75:
-                            title = title[:72] + " . . ."
-                        else:
-                            magic_number = 78 - len(title)
-                            magic_string = " " * magic_number
-                            title = title + magic_string
-                        if len(display_author) > 26:
-                            display_author = display_author[:23] + " . . ."
-                        display_string = "{}| {} | {}".format(number, title, display_author)
+                        display_string = string_handler.display_string(article_id, width)
                         if cursor_y == y_position:
                             stdscr.attron(curses.color_pair(3))
                         stdscr.addstr(y_position, x_start_pos, display_string)
                         if cursor_y == y_position:
                             stdscr.attroff(curses.color_pair(3))
-                elif current_page == max_pages:
+                elif current_page == num_pages:
                     status_bar = " 'p' : Previous Page | 'o' : Open | 'i' : Info | esc : Issue Selection | 'v': Volume Selection | 'm' : Main Menu "
                     for i in range(0, max_rows + 1):
                         y_position = i + 1
                         check = i + (max_rows * (current_page-1)) - 1
                         if not check > rows:
-                            article_id = article_ids[i + (max_rows * (current_page-1)) - 2]
-                            number = get_numbers.full(article_id)
-                            number = number.split(".")[1] + "." + number.split(".")[2]
-                            title = get_title.by_article_id(article_id)
-                            author = get_author.by_article_id(article_id)
-                            display_author = names.get_authors(author)
-                            if len(title) > 75:
-                                title = title[:72] + " . . ."
-                            else:
-                                magic_number = 78 - len(title)
-                                magic_string = " " * magic_number
-                                title = title + magic_string
-                            if len(author) > 25:
-                                author = author[:22] + " . . ."
-                            display_string = "{}| {} | {}".format(number, title, display_author)
+                            index = arith.get_index(max_rows, num_pages, current_page, y_position, False)
+                            article_id = article_ids[index]
+                            display_string = string_handler.display_string(article_id, width)
                             if cursor_y == y_position:
                                 stdscr.attron(curses.color_pair(3))
                             stdscr.addstr(y_position, x_start_pos, display_string)
@@ -151,42 +122,14 @@ def menu(stdscr, volume_number, year, issue_number, volume_current_page):
                         y_position = i + 1
                         check = i + (max_rows * (current_page-1)) - 1
                         if not check > rows:
-                            article_id = article_ids[i + (max_rows * (current_page-1)) - 2]
-                            number = get_numbers.full(article_id)
-                            number = number.split(".")[1] + "." + number.split(".")[2]
-                            title = get_title.by_article_id(article_id)
-                            author = get_author.by_article_id(article_id)
-                            display_author = names.get_authors(author)
-                            if len(title) > 75:
-                                title = title[:72] + " . . ."
-                            else:
-                                magic_number = 78 - len(title)
-                                magic_string = " " * magic_number
-                                title = title + magic_string
-                            if len(author) > 25:
-                                author = author[:22] + " . . ."
-                            display_string = "{}| {} | {}".format(number, title, display_author)
+                            index = arith.get_index(max_rows, num_pages, current_page, y_position, False)
+                            article_id = article_ids[index]
+                            display_string = string_handler.display_string(article_id, width)
                             if cursor_y == y_position:
                                 stdscr.attron(curses.color_pair(3))
                             stdscr.addstr(y_position, x_start_pos, display_string)
                             if cursor_y == y_position:
                                 stdscr.attroff(curses.color_pair(3))
-                i = cursor_y - 1
-                if current_page == max_pages:
-                    article_id = article_ids[i + (max_rows * (current_page -1)) - 2]
-                elif(current_page == 1):
-                    article_id = article_ids[i]
-                else:
-                    article_id = article_ids[i + (max_rows * (current_page -1)) - 1]
-                if k == ord('o') or k == 10:
-                    full_number = get_numbers.full(article_id)
-                    open.open_file(full_number)
-                elif k == ord('i'):
-                    article.start(article_id, volume_number, year, issue_number, volume_current_page)
-                elif k == ord('m'):
-                    main.start()
-                elif k == ord('v'):
-                    by_volume_year.start(volume_current_page)
             else:
                 l_row = "Page {} of {}".format(current_page, num_pages)
                 if k == curses.KEY_LEFT:
@@ -194,35 +137,23 @@ def menu(stdscr, volume_number, year, issue_number, volume_current_page):
                 for i in range(0, rows - 1):
                     y_position = i + 1
                     article_id = article_ids[i]
-                    number = get_numbers.full(article_id)
-                    number = number.split(".")[1] + "." + number.split(".")[2]
-                    title = get_title.by_article_id(article_id)
-                    author = get_author.by_article_id(article_id)
-                    display_author = names.get_authors(author)
-                    if len(title) > 75:
-                        title = title[:72] + " . . ."
-                    else:
-                        magic_number = 78 - len(title)
-                        magic_string = " " * magic_number
-                        title = title + magic_string
-                    display_string = "{}| {} | {}".format(number, title, display_author)
+                    display_string = string_handler.display_string(article_id, width)
                     if cursor_y == y_position:
                         stdscr.attron(curses.color_pair(3))
                     stdscr.addstr(y_position, x_start_pos, display_string)
                     if cursor_y == y_position:
                         stdscr.attroff(curses.color_pair(3))
-                i = cursor_y - 1
-                article_id = article_ids[i]
-                if k == ord('o') or k == 10:
-                    full_number = get_numbers.full(article_id)
-                    open.open_file(full_number)
-                elif k == ord('m'):
-                    main.start()
-                elif k == ord('v'):
-                    by_volume_year.start(current_page)
-                elif k == ord('i'):
-                    article.start(article_id, volume_number, year, issue_number, volume_current_page)
-
+            i = arith.get_index(max_rows, num_pages, current_page, cursor_y, False)
+            article_id = article_ids[i]
+            if k == ord('o') or k == 10:
+                full_number = get_numbers.full(article_id)
+                open.open_file(full_number)
+            elif k == ord('i'):
+                article.start(article_id, volume_number, year, issue_number, volume_current_page)
+            elif k == ord('m'):
+                main.start()
+            elif k == ord('v'):
+                by_volume_year.start(current_page)
         stdscr.attron(curses.color_pair(3))
         stdscr.addstr(height-1, 0, status_bar)
         stdscr.addstr(height-1, len(status_bar), " " * (width - len(status_bar) - 1))
