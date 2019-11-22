7a714a73a221db850768bcf046d2205f632e74b4
Jonathan Thorne <jonathanthorne13@gmail.com>
2019-11-22 13:28:56 -0500

    updated variable names to not conflict

--- utilities/sort_dict.py
+++ utilities/sort_dict.py
@@ -17,47 +17,50 @@ def create(list):
         else:
             last_location = len(full_name) - 1
         last = full_name[last_location]
-        if not last_location == 1:
-            middle_full = get_middle(last_location, full_name, [])
+        if not last_location == 1 or last_location == 0:
+            middle_full = get_middle(last_location, full_name, [], first)
             middle = ' '.join(middle_full)
             middle = " " + middle + " "
         name_dict = {"first":first, "middle":middle, "last":last, "post":postfix}
         dict_list.append(name_dict)
     return dict_list
 
-def sort(list, num):
+def sort(lis, num):
     if num == 1:
-        return sorted(list, key=itemgetter('last'))
+        return sorted(lis, key=itemgetter('last'))
     elif num == 2:
-        return sorted(list, key=itemgetter('last'), reverse = True)
+        return sorted(lis, key=itemgetter('last'), reverse = True)
     elif num == 3:
-        return sorted(list, key=itemgetter('first'))
+        return sorted(lis, key=itemgetter('first'))
     elif num == 4:
-        return sorted(list, key=itemgetter('first'), reverse = True)
+        return sorted(lis, key=itemgetter('first'), reverse = True)
 
-def get_middle(location, full_name, middle_full):
+def get_middle(location, full_name, middle_full, first):
     middle_end = location - 1
     middle = full_name[middle_end]
     if not len(middle) == 2:
-        middle = middle[0:1] + "."
-        full_name[middle_end] = middle
-        middle_full.append(middle)
-        if not middle_end <= 1:
-            return get_middle(middle_end, full_name, middle_full)
+        if not len(first) == 2:
+            middle = middle[0:1] + "."
+            full_name[middle_end] = middle
+            middle_full.append(middle)
+            if not middle_end <= 1:
+                return get_middle(middle_end, full_name, middle_full, first)
+        else:
+            middle_full.append(middle)
     else:
         middle_full.append(middle)
     return middle_full
 
-def get_name(dict):
-    first = dict["first"]
-    middle = dict["middle"]
-    post = dict["post"]
-    last = dict["last"]
+def get_name(dic):
+    first = dic["first"]
+    middle = dic["middle"]
+    post = dic["post"]
+    last = dic["last"]
     author = first + middle + last + post
     return author
 
-def get_all_names(list):
+def get_all_names(lis):
     names = []
-    for dict in list:
-        names.append(get_name(dict))
+    for dic in lis:
+        names.append(get_name(dic))
     return names
