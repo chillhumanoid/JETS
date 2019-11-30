f84ec4298477668583c59cc430467a0bf18d3fcd
Jonathan Thorne <jonathanthorne13@gmail.com>
2019-11-30 09:49:27 -0500

    Changed location of main menu
    Won't be seen just yet

--- jets.py
+++ jets.py
@@ -1,5 +1,3 @@
-from menus import main as main_menu                                        #outside imports
-
-
+from display import  main as main_menu                                    #outside imports
 if __name__ == '__main__':
     main_menu.start()
