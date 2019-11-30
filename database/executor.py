898ecd0985a62f21e1e9593a979c9af621df6992
Jonathan Thorne <jonathanthorne13@gmail.com>
2019-11-30 09:43:02 -0500

    Moved to self hosted DB
    Also moved from sqlite to mysql bc tha tmakes sense

--- database/executor.py
+++ database/executor.py
@@ -1,20 +1,14 @@
-import sqlite3
+import mysql.connector
 
-path = "C:/Users/jonat/JETS/Articles/"
-def execute(sql):
-    """
-    Executes SQL code
-
-    Instead of executing in each function, executes in this base function
-
-    Parameters:
-    sql (string): SQL query to be executed
-
-    Returns:
-    c (sql query) : whatever was in the query, gets returned to the function that called it
-    """
-    conn = sqlite3.connect(path + "author.db")
-    c = conn.cursor()
-    c.execute(sql)
-    conn.commit()
-    return c
+def select(sql):             
+    mydb = mysql.connector.connect(
+    host="192.168.1.171",
+    user="jets-util",
+    passwd="utilityaccount",                 #Because this is a utility account that only has permission to run SELECT queries, i dont really care.
+    database="jets"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    mydb.commit()
+    return myresult
