c1e58878c21035242f0944b754406e2399362fe6
Jonathan Thorne <jonathanthorne13@gmail.com>
2019-11-30 09:58:01 -0500

    moved file, added options, started work on some options

--- display/main.py
+++ display/main.py
@@ -1,8 +1,6 @@
 import curses, sys
-from display import displays, login_screen as login
-from download import get_url
-from utilities import login as log, variables as var
-from utilities.menu import title, add_string, menu_option, status_bar, get_height, get_width
+from display import displays, login_screen as login, download_location as dl
+from utilities import variables as var, menu as m
 def main_menu(stdscr):
     cursor_y = var.main_position
     cursor_x = 1
@@ -13,7 +11,6 @@ def main_menu(stdscr):
     stdscr.clear()
     stdscr.refresh()
 
-    isLogged = log.check_login()
     height, width = stdscr.getmaxyx()
 
     curses.start_color()
@@ -25,27 +22,29 @@ def main_menu(stdscr):
     status_msg = "Written by Jonathan Thorne | © 2019 | Press 'esc' to quit"
     option_1 = "1. View by Volume/Year"
     option_2 = "2. View by Author"
-    option_3 = "3. View by Topic (Not Done Yet)"
-    option_4 = "4. Check for New Articles"
-    option_5 = "5. Login"
-    option_5_b = "Logged in to etsjets.org"
+    option_3 = "3. View by Topic (NOT DONE)"
+    option_4 = "4. Search (NOT DONE)"
+    option_5 = "5. Downloads (WIP)"
+    option_6 = "6. Login"
+    option_6_b = "Logged in to etsjets.org"
 
     while(True):
         stdscr.clear()
 
-        title(stdscr, title_str)
+        m.title(stdscr, title_str)
 
-        status_bar(stdscr, status_msg)
+        m.status_bar(stdscr, status_msg)
 
-        menu_option(stdscr, option_1, 1, 1, cursor_y)
-        menu_option(stdscr, option_2, 2, 1, cursor_y)
-        menu_option(stdscr, option_3, 3, 1, cursor_y)
-        menu_option(stdscr, option_4, 4, 1, cursor_y)
+        m.menu_option(stdscr, option_1, 1, 1, cursor_y)
+        m.menu_option(stdscr, option_2, 2, 1, cursor_y)
+        m.menu_option(stdscr, option_3, 3, 1, cursor_y)
+        m.menu_option(stdscr, option_4, 4, 1, cursor_y)
+        m.menu_option(stdscr, option_5, 5, 1, cursor_y)
 
-        if isLogged == False:
-            menu_option(stdscr, option_5, 5, 1, cursor_y)
+        if var.isLogged == False:
+            m.menu_option(stdscr, option_6, 6, 1, cursor_y)
         else:
-            menu_option(stdscr, option_5_b, 6, 1, cursor_y)
+            m.menu_option(stdscr, option_6_b, 7, 1, cursor_y)
 
         stdscr.move(cursor_y, cursor_x)
         stdscr.refresh()
@@ -54,16 +53,20 @@ def main_menu(stdscr):
             sys.exit()
         elif k == curses.KEY_UP:
             cursor_y -= 1
-            if cursor_y == 0 and isLogged == False:
-                cursor_y = 5
-            elif cursor_y == 0 and isLogged == True:
-                cursor_y = 4
+            if cursor_y == 4: #COMMENT THIS OUT LATER
+                cursor_y = 2 #DITTO
+            elif cursor_y == 0 and var.isLogged == False:
+                cursor_y = 6
+            elif cursor_y == 0 and var.isLogged == True:
+                cursor_y = 5 # CHANGE THIS
         elif k == curses.KEY_DOWN:
             cursor_y += 1
-            if cursor_y == 6 and isLogged == False:
+            if cursor_y > 6 and var.isLogged == False:
                 cursor_y = 1
-            elif cursor_y == 5 and isLogged == True:
-                cursor_y = 1
+            elif cursor_y > 3 and var.isLogged == True: #CHANGE THIS
+                cursor_y = 5
+            elif cursor_y == 3 and var.isLogged == False: # COMMENT THIS OUT
+                cursor_y = 5 #COMMENT THIS OUT
         elif k == 10 or k == curses.KEY_RIGHT:
             char = int.from_bytes(stdscr.instr(cursor_y, 1, 1), byteorder='little')
             if char == ord('1'):
@@ -76,9 +79,9 @@ def main_menu(stdscr):
                 var.menu_type = "authors"
                 var.author_current_page = 1
                 displays.start()
-            elif char == ord('4'):
-                get_url.volume()
             elif char == ord('5'):
+                dl.start()
+            elif char == ord('6'):
                 login.start(cursor_y)
         elif k == 260:
             start(cursor_y)
