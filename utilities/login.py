230564bda017757c9c038d47aac5752cde12c629
Jonathan Thorne <jonathanthorne13@gmail.com>
2019-11-30 09:52:48 -0500

    Updated login logic

--- utilities/login.py
+++ utilities/login.py
@@ -1,10 +1,8 @@
-import util, os
 from bs4 import BeautifulSoup
-from requests import *
+from pathlib import Path
+from requests import Session
 
-path = os.path.realpath(__file__)
-path = path.replace("utilities\\login.py","")
-
+path = Path("login.txt")
 
 def set_login(user, pwd):
     with Session() as s:
@@ -15,29 +13,23 @@ def set_login(user, pwd):
         if(len(login_check) > 0):
             return False
         else:
-            f = open(path + "login.txt", "w+")
+            f = open(path, "w")
             f.write(user + "\n")
             f.write(pwd + "\n")
+            f = open(path, "")
             return True
 
 def get_login():
-    f = open(path + "login.txt", "r")
-    lines = f.readlines()
-    return lines
+    if path.exists():
+        f = open(path, "r")
+        lines = f.readlines()
+        return lines
+    else:
+        return []
 
 def check_login():
     lines = get_login()
     if not len(lines) == 0:
         return True
     else:
-        return False 
-
-def get_password():
-    lines = get_login()
-    pwd = lines[1]
-    return pwd
-
-def get_username():
-    lines = get_login()
-    username = lines[0]
-    return username
+        return False
